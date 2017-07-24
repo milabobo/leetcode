5d25ca7085e3a1091013b925540919a15fbd5be3
milabobo <milabobo@126.com>
2017-07-24 22:54:35 +0800

    add template

---
+++ p1/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by milabobo on 10:47 PM.
+ * Email mailto:milabobo@126.com
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("P1");
+    }
+}
---
+++ p2/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by milabobo on 10:48 PM.
+ * Email mailto:milabobo@126.com
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("P2");
+    }
+}
