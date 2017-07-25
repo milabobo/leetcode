033fa44e75e3ecdaf60689640d15e4e0eaf6bd2a
milabobo <milabobo@126.com>
2017-07-25 14:52:13 +0800

    add p8

--- p8/src/Main.java
+++ p8/src/Main.java
@@ -8,10 +8,15 @@ public class Main {
         System.out.println(solution.myAtoi("100"));
         System.out.println(solution.myAtoi("-c1001"));
         System.out.println(solution.myAtoi("-100100000000"));
+        System.out.println(solution.myAtoi("   -100 1"));
+        System.out.println(solution.myAtoi("-0012a42"));
+        System.out.println(solution.myAtoi("-"));
+        System.out.println(solution.myAtoi("+"));
+        System.out.println(solution.myAtoi("2147483648"));
     }
 
     public static class Solution {
-        static int INVALID = -1;
+        static int INVALID = 0;
         static int INTEGER_MAX = Integer.MAX_VALUE;
         static int INTEGER_MIN = Integer.MIN_VALUE;
 
@@ -20,23 +25,34 @@ public class Main {
                 return INVALID;
             }
 
-            char first = str.charAt(0);
+            int firstPosition = 0;
+            while (firstPosition < str.length()
+                    && str.charAt(firstPosition) == ' ') {
+                firstPosition++;
+            }
+
+            char first = str.charAt(firstPosition);
             if (first != '-' && first != '+' && !isDigit(first)) {
                 return INVALID;
             }
 
             int sign = first == '-' ? -1 : 1;
             int result = 0;
-            int position = first == '-' || first == '+' ? 1 : 0;
+            int position = first == '-' || first == '+'
+                    ? firstPosition + 1 : firstPosition;
+
+            if (position < str.length() && !isDigit(str.charAt(position))) {
+                return INVALID;
+            }
 
             while (position < str.length()) {
                 char a = str.charAt(position);
                 if (!isDigit(a)) {
-                    return INVALID;
+                    break;
                 }
                 int temp = result * 10 + value(a);
                 // check overflow
-                if ((temp - value(a)) / 10 != result) {
+                if (temp < 0 || (temp - value(a)) / 10 != result) {
                     return sign == 1 ? INTEGER_MAX : INTEGER_MIN;
                 }
                 result = temp;
