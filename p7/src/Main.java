dc5d11ad682eaa9b54740d24b38da35c1ccd0f5f
milabobo <milabobo@126.com>
2017-07-25 12:13:13 +0800

    modify p7 code

--- p7/src/Main.java
+++ p7/src/Main.java
@@ -7,28 +7,26 @@ public class Main {
         Solution solution = new Solution();
         System.out.println(solution.reverse(1534236469));
         System.out.println(solution.reverse(100345));
-        System.out.println(solution.reverse(2147483647));
+        System.out.println(solution.reverse(-100345));
     }
 
     public static class Solution {
         public int reverse(int x) {
-            boolean isNegative = x < 0;
-            int absX = isNegative ? -x : x;
-            int sum = absX % 10;
-            int k = absX;
+            int sum = x % 10;
+            int k = x;
             while (k / 10 != 0) {
                 int tempSum = sum * 10;
                 if (tempSum / 10 != sum) {
                     return 0;
                 }
                 int tempSum1 = tempSum + (k / 10) % 10;
-                if (tempSum1 < tempSum) {
+                if (tempSum1 - tempSum != (k / 10) % 10) {
                     return 0;
                 }
                 sum = tempSum1;
                 k = k / 10;
             }
-            return isNegative ? -sum : sum;
+            return sum;
         }
     }
 }
